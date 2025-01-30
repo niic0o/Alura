f49bc82f6619a9d0db833a1aff98bfb8d7ce87e8
nico <nicosena10@gmail.com>
2025-01-30 16:31:32 -0300

    curso_02_backup

--- Logica/curso02/Dos/app.js
+++ Logica/curso02/Dos/app.js
@@ -1,11 +1,27 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Adivina el número secreto';
-
-let parrarfo = document.querySelector('p');
+addTextToSelector('h1', 'Adivina el número secreto');
 let max = 10;
+let numeroSecreto = generarNumeroSecreto();
+addTextToSelector('p', `El número secreto está entre 1 y ${max}`);
 
-parrarfo.innerHTML = `El número secreto está entre 1 y ${max}`;
+/*hoisting en accion*/
+/**
+ * @param {string} p_selector recibe la etiqueta HTML
+ * @param {string} p_texto recibe el texto para unir al selector
+ */
+function addTextToSelector(p_selector,p_texto){
+    try {
+        let selector = document.querySelector(p_selector);
+        selector.innerHTML = p_texto;        
+    } catch (error) {
+        console.error('Error al agregar texto a selector: ', error);        
+    }
+
+};
 
 function intentoDeUsuario(){
     alert('Ha presionado sobre el intento');
-}
+};
+
+function generarNumeroSecreto(){
+    return Math.floor(Math.random() * max) + 1;
+};
